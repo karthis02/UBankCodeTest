c1ec353d9477fa362b764780dd86584ef5f4de8b
Karthi  S S <karthiss@Karthis-MacBook-Pro.local>
2021-06-05 21:29:20 +1000

    Initial Commit

---
+++ UBankiOS/UBankiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UBankiOS
+//
+//  Created by Karthi  S S on 5/6/21.
+//  Copyright © 2021 Karthi  S S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
